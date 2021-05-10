b1bc51079882896363946dc3d27e82c124107d1d
Danielle Martinez <daniellealexandria10@gmail.com>
2021-05-10 14:23:05 -0600

    render gods

--- src/components/GodListItem/GodListItem.jsx
+++ src/components/GodListItem/GodListItem.jsx
@@ -1,7 +1,9 @@
 export default function GodListItem({god}) {
     return (
-      <div className="item">
-      <div className="pic flex-ctr-ctr">{god.name}</div>
+      <div className="god">
+          <img src={god.filename} alt="god icon"/>
+      <div className="name flex-ctr-ctr">{god.name}</div>
+      <div className="name flex-ctr-ctr">{god.type}</div>
       </div>
     );
   }
