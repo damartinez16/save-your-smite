1aed18df2e191ae03f335b928fa977013c71836e
Danielle Martinez <daniellealexandria10@gmail.com>
2021-05-11 15:08:57 -0600

    build detail code

--- src/components/BuildDetail/BuildDetail.jsx
+++ src/components/BuildDetail/BuildDetail.jsx
@@ -1,3 +1,10 @@
-export default function BuildDetail() {
-    return <h1>build detail</h1>
+export default function BuildDetail({build}) {
+    return (
+        <>
+        <h1>details</h1>
+        {build.items.map((item) => 
+        <div>{item}</div>
+        ) }
+        </>
+    )
 }
